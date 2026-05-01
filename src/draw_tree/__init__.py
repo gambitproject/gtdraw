6a5fab83a76cf723b7b9499127cfb820f71ec87e
Ed Chalstrey <edwardchalstrey@gmail.com>
2026-05-01 15:07:21 +0100

    Bump version to 0.6.0

--- src/draw_tree/__init__.py
+++ src/draw_tree/__init__.py
@@ -5,7 +5,7 @@ This package provides functionality to generate TikZ code for game trees
 from extensive form (.ef) files, with support for Jupyter notebooks.
 """
 
-__version__ = "0.5.1"
+__version__ = "0.6.0"
 
 from .core import (
     draw_tree,
